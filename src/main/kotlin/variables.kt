59d9e9551f348a90f2ea7c034978e4c95331e617
VictorH <victorhenzzowp@gmail.com>
2023-09-16 15:34:49 -0300

    Adding variables file

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var number1: Int = 1; // variable at runtime
+    val number2: Int = 2; // constant at runtime
+    val sum: Int = number1+number2;
+
+    println("$number1 + $number2 = $sum");
+}
